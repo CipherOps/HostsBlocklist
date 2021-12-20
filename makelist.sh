1714bc1bcacc1f686e79c8636a298d735bd37ee6
Tyler <53319122+CipherOps@users.noreply.github.com>
2021-12-19 20:04:26 -0500

    Update makelist.sh

--- makelist.sh
+++ makelist.sh
@@ -2,6 +2,7 @@ rm -rf all-lists
 rm -rf merged.txt
 mkdir -p all-lists
 wget -nd -P all-lists -i sources.txt
-cat all-lists/* | grep -Ev '^([0-9]{1,3}(\.[0-9]{1,3}){3}|[^a-zA-Z0-9].*)$' | sort | uniq > merged.txt
-cat merged.txt | awk '{print $NF}' > hosts.txt
-rm merged.txt
+cat all-lists/* | grep -Ev '^([0-9]{1,3}(\.[0-9]{1,3}){3}|[^a-zA-Z0-9].*)$' | sort | uniq -u > merged.txt
+cat merged.txt | awk '{print $NF}' | sort | uniq -u > hosts.txt
+rm -rf merged.txt
+rm -rf all-lists
